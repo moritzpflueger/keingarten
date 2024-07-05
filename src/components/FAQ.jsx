01768119fc15814b877b26747ccce1c0e91970b3
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-07-05 17:14:24 +0200

    polish FAQ

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,19 +1,47 @@
+import IconInfo from "./IconInfo";
+import IconQuestionmark from "./IconQuestionmark";
+
 const FAQ = () => {
   return (
-    <section className="p-5">
-      <h1 className="text-8xl">FAQ</h1>
-      <details>
-        <summary>What is React?</summary>
-        <p>React is a JavaScript library for building user interfaces.</p>
-      </details>
-      <details>
-        <summary>What is JSX?</summary>
-        <p>JSX is a syntax extension for JavaScript.</p>
-      </details>
-      <details>
-        <summary>What is Babel?</summary>
-        <p>Babel is a JavaScript compiler.</p>
-      </details>
+    <section className="p-5 max-w-3xl">
+      <h1 
+        className="font-serif text-7xl my-5"
+        style={{ fontFamily: 'Phase' }}
+      >
+        <span className="pr-3">From A to Be</span>
+      </h1>
+      <div className="flex flex-col gap-5">
+        <div className="flex gap-5">
+          <div className="w-20">
+            <IconQuestionmark />
+          </div>
+          <p>Frequently asked questions are listed below. Please feel free to contact any time if something is unclear to you?</p>
+        </div>
+        <div className="flex gap-5">
+          <div className="w-20">
+            <IconInfo />
+          </div>
+          <p>Please let us know if you need help accessing the garden. We provide a pick up for older or disabled persons.</p>
+        </div>
+      </div>
+      <div className="px-1 mt-5">
+        <details>
+          <summary>How to get there?</summary>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        </details>
+        <details>
+          <summary>Wheelchar Accessibility?</summary>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        </details>
+        <details>
+          <summary>Taking Care?</summary>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        </details>        
+        <details>
+          <summary>Staying overnight?</summary>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        </details>        
+      </div>
       
     </section>
   )
