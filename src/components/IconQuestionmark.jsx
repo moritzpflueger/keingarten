9681b0ae5298f1f3c49e40ef8b4f91757bdb48fa
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-07-05 17:15:21 +0200

    introduce icons

---
+++ src/components/IconQuestionmark.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const IconQuestionmark = () => {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="12" cy="12" r="12" fill="black" />
+      <text
+        x="50%"
+        y="55%"
+        textAnchor="middle"
+        dominantBaseline="middle"
+        fontSize="16"
+        fill="white"
+        fontFamily="Jetbrains Mono"
+      >
+        ?
+      </text>
+    </svg>
+  );
+};
+
+export default IconQuestionmark;
