b5de96713a15e54c3b062781a4d0142e91daaeef
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-07-05 17:15:46 +0200

    polish landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,40 @@
 import './App.css'
 import { Link } from 'react-router-dom'
 import test1 from './assets/test1.webp'
+import Registration from './components/Registration'
+import IconQuestionmark from './components/IconQuestionmark'
+import IconInfo from './components/IconInfo'
 
 function App() {
 
   return (
-      <section 
-        className="
-          flex-1
-          relative
-          my-8
-          mx-4
-          font-mono
-          font-semibold
-          dotted-background
-        "
-      >
-        <div 
-          className="flex items-center justify-center w-full h-full absolute"
-        >
-          <h1 className="relative z-10 text-[clamp(2rem,_24vw,_10rem)] leading-[1.2] font-serif font-bold -translate-y-8">
-            From <br/> A to <br/> Be
-          </h1>
-          <div className="absolute">
-            <img src={test1} />
+      <section>
+        <div className="p-10 grid grid-cols-1 md:grid-cols-2 gap-10">
+          <div className="col-span-1">
+            <h1 
+              className="font-serif text-7xl"
+              style={{ fontFamily: 'Phase' }}
+            >
+              <span className="pr-3">From A <br/> to Be</span>
+              <div className="inline-flex whitespace-nowrap">
+                <Link to ="/" className="mx-3 w-16">
+                  <IconQuestionmark />
+                </Link>
+                <Link to ="/" className="mx-3 w-16">
+                  <IconInfo />
+                </Link>                
+              </div>
+            </h1>
+            <p className="text-2xl my-10">01.08.2024 - 12.08.2024</p>
+            <p className="text-2xl">
+              Workshops with <Link className="whitespace-nowrap underline" to="/">Ula Liagaite</Link>, 
+              <br/><Link className="whitespace-nowrap underline" to="/">AnttoLogy</Link>, <Link className="whitespace-nowrap underline" to="/">Tatiana Heumann</Link> 
+              <br/> and <Link className="whitespace-nowrap underline" to="/">Florencia Curci</Link>
+            </p>
           </div>
-        </div>
-
-        <div className="absolute -top-1 left-0 bg-white pr-1">
-          aktuell
-        </div>
-        <div className="absolute -top-1 -right-2 bg-white px-2">
-          O1.O8.2O24-12.O8.2O24
-        </div>
-        <div className="absolute bottom-11 left-0">
-          <span className="bg-white pr-1">mit Ula Liagaite</span> <br />
-          <span className="bg-white pr-1">Tatiana Heumann</span> <br />
-          <span className="bg-white pr-1">Florencia Curci</span> <br />
-        </div>
-        <div className="absolute -bottom-1 -right-2 bg-white px-2">
-          <Link to="/impressum">weitere Informationen</Link>
+          <div className="col-span-1 flex justify-start mt-3">
+            <Registration />
+          </div>
         </div>
 
       </section>  
