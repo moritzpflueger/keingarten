fa17256a17800e6cf90c43329e059c5f8af9c74c
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-07-08 13:17:08 +0200

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,27 @@
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+
 import iconInstagram from '../assets/iconInstagram.svg';
 import LogoLHS from '../assets/LHS_Badge_mitZusatz_sw_Print.png';
 import LogoTNC from '../assets/TNClogoPrimary_OU_CMYK_Europe.png';
 
 const Footer = () => {
+
+  const { t } = useTranslation();
+
   return (
     <footer className="bg-[lime] p-10">
-      <p>
-        Curatorial concept and implementation: Kirill Agafonov, Florian Amoser, Jonas Bernetta, Sharon Chan, Alisha Dutt Islam, Nistiman Erdede, Hannah Essler, Micol Favini, Tsz Hei Fung, Christian Gieben, Emanuel Haab, Roni Idrizaj, Charles Kwong, Ludwig Lederer, Sébastien Mitra, Engy Mohsen, Hans-Jakob Mühlethaler, Jan Reimann, Hiba Tahhan, Soraya Tashima, Anastasia Tatarenko, Egor Tatarenko. Supervision and support: Nicole Frei, Simon Grab, Patrick Müller, Nicholas Schärer, Irene Vögeli       
-      </p>
-      <div className="flex flex-wrap items-center justify-between py-10 order-1 gap-5">
-        <Link to="/legal" >legal notice</Link>
-        <div className="flex mx-auto pt-8 sm:pt-0 gap-5 items-center sm:justify-center order-3 sm:order-2">
-
-          <a href="https://www.instagram.com/keingartenxyz/" target="_blank">
-            <img src={iconInstagram} alt="Instagram" className="w-10" />   
-          </a>
-
-          {/* TODO: Add icon and link to ZHDK? */}
-          {/* https://www.zhdk.ch/ */}
-
-        </div>
-        <Link to="/impressum" className="order-2 sm:order-3">impressum</Link>
+      <div className="flex flex-col">
+        <Link to={'/about'} className="hover:underline">{ t('menu.about') }</Link>
+        <Link to={'/#registration'} className="hover:underline">{ t('menu.registration') }</Link>
+        <Link to={'/#program'} className="hover:underline">{ t('menu.program') }</Link>
+        <Link to={'/#faq'} className="hover:underline">{ t('menu.faq') }</Link>
+        <Link to={'/contact'} className="hover:underline">{ t('menu.contact') }</Link>
+        <a href="https://www.instagram.com/keingartenxyz/" target="_blank" className="mt-10">
+          <img src={iconInstagram} alt="Instagram" className="w-10" />   
+        </a>        
+        <Link to="/impressum" className="hover:underline mt-10">impressum</Link>
+        <Link to="/legal" className="hover:underline">legal notice</Link>
       </div>
       <div className='flex flex-wrap gap-20 w-full my-10'>
         <a 
