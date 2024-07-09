16d3cff5978e7e807b052e813dddfbe4760520bc
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-07-09 16:34:45 +0200

    update program translations

--- src/components/Programme.jsx
+++ src/components/Programme.jsx
@@ -6,6 +6,7 @@ import InfoBox from "./InfoBox";
 const Programme = () => {
   
   const { t } = useTranslation();
+  const program = t('program.items', { returnObjects: true });
   
   return (
     <section id="program" className="p-5 mx-auto max-w-4xl">
@@ -36,18 +37,35 @@ const Programme = () => {
         </InfoBox>              
       </div>
 
-      {new Array(10).fill().map((item, index) => (
-      <article key={`program_${index}`} className="mb-10">
-        <p className="mt-5 text-xl">
-          <strong> 01.08.2024 - 07.08.2024,</strong> daily between 16:00-18:00h
-          <br/><i className="font-semibold">A move towards a fiction of coexistence</i>
-          <br/>with <Link className="whitespace-nowrap underline">Ula Liagaite</Link> and <Link className="whitespace-nowrap underline">AnttoLogy</Link>
-        </p>
-        <p className="mt-5 max-w-2xl">
-          An action, a move, a collective effort to dig deeper in an ongoing practice. It invites those at keingarten who want to move, play, build and listen together, while being guided through movement practice composed of simple physical exercises and tasks. We meet on a daily basis, get our hands dirty and build a swing?
-        </p>
-      </article>        
-      ))}         
+      {program.map((item, index) => (
+        <article key={`program_${index}`} className="mb-10">
+          <p className="mt-5 text-xl">
+            <Trans
+              i18nKey="program.items.date"
+              components={{
+                strong: <strong />,
+              }}
+            >
+              {item.date}
+            </Trans>
+            <br />
+            <i className="font-semibold">{item.title}</i>
+            <br />
+            <Trans
+              i18nKey="program.items.description"
+              components={{
+                1: <Link to={t(item.link1)} className="whitespace-nowrap underline" />,
+                3: item.link2 ? <Link to={item.link2} className="whitespace-nowrap underline" /> : <span />,
+              }}
+            >
+              {item.description}
+            </Trans>
+          </p>
+          <p className="mt-5 max-w-2xl">
+            {t(item.text)}
+          </p>
+        </article>
+      ))}
     </section>
   )
 }
