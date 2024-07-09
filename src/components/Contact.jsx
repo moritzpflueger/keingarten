1ba33f49bacb836a7cdc443688c6fbd9a2296ecd
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-07-09 14:49:37 +0200

    add instagram and whatsapp to contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 
+import iconInstagram from '../assets/iconInstagram.svg';
+import iconWhatsapp from '../assets/iconWhatsapp.svg';
+
 const Contact = () => {
 
   const { t } = useTranslation();
@@ -19,9 +22,17 @@ const Contact = () => {
           { t('contact.title') }
         </span>
       </h1>
-      <p>
+      <p className="mb-5">
         Mail: <a className="underline" href="mailto:keingarten.xyz@gmail.com">keingarten.xyz@gmail.com</a>
       </p>
+      <div className="flex gap-5">
+        <a className="underline" href="https://chat.whatsapp.com/EKrevz698XsGSmPkDYo74V">
+          <img src={iconWhatsapp} alt="Whatsapp" className="w-11 h-11 inline-block" />
+        </a>
+        <a className="underline" href="https://www.instagram.com/keingarten.xyz/">
+          <img src={iconInstagram} alt="Instagram" className="w-12 h-12 inline-block" />
+        </a>        
+      </div>
       
     </section>
   )
