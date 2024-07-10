90e2742050456b003e91f05a902614a71a83bf23
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-07-10 13:52:14 +0200

    polish footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 import iconInstagram from '../assets/iconInstagram.svg';
+import iconWhatsapp from '../assets/iconWhatsapp.svg';
 import LogoLHS from '../assets/LHS_Badge_mitZusatz_sw_Print.png';
 import LogoTNC from '../assets/TNClogoPrimary_OU_CMYK_Europe.png';
 
@@ -10,15 +11,32 @@ const Footer = () => {
   const { t } = useTranslation();
 
   return (
-    <footer className="bg-[lime] p-10 font-semibold">
+    <footer className="bg-[lime] p-10 font-semibold flex flex-wrap justify-between items-center">
       {/* <div className="flex flex-col">
         <Link to={'/about'} className="hover:underline">{ t('menu.about') }</Link>
         <Link to={'/#registration'} className="hover:underline">{ t('menu.registration') }</Link>
         <Link to={'/#program'} className="hover:underline">{ t('menu.program') }</Link>
         <Link to={'/#faq'} className="hover:underline">{ t('menu.faq') }</Link>
         <Link to={'/contact'} className="hover:underline">{ t('menu.contact') }</Link>
-      </div> */}
-      <div className='flex flex-wrap justify-center gap-5 md:gap-10 w-full mb-10'>
+      </div> */}    
+      <div className="flex items-center justify-between md:gap-16 w-full md:w-auto">
+        <p className="flex flex-col">
+          <span className="mb-5 whitespace-nowrap">
+            © {new Date().getFullYear()} Keingarten
+          </span>
+          <Link to="/impressum" className="hover:underline">impressum</Link>
+          <Link to="/legal" className="hover:underline">legal notice</Link>                 
+        </p>
+        <div className="flex gap-5 items-center">
+          <a href="https://www.instagram.com/keingartenxyz/" target="_blank" className="block">
+            <img src={iconInstagram} alt="Instagram" className="w-12 md:w-16" />   
+          </a>        
+          <a href="https://chat.whatsapp.com/EKrevz698XsGSmPkDYo74V" target="_blank" className="block sm:hidden">
+            <img src={iconWhatsapp} alt="Whatsapp" className="w-10" />   
+          </a>             
+        </div>
+      </div>
+      <div className='flex justify-center gap-5 mt-10 w-full md:w-auto'>
         <a 
           href="https://www.stuttgart.de/leben/umwelt/klima/klimastrategie/klima-aktionsprogramm/stuttgarter-klima-innovationsfonds/" 
           target="_blank" 
@@ -33,17 +51,7 @@ const Footer = () => {
         >
           <img src={LogoTNC} alt="LHS Logo" className="max-w-[160px]" />
         </a>
-      </div>      
-      <div className="flex flex-col md:flex-row md:items-center md:gap-10">
-        <a href="https://www.instagram.com/keingartenxyz/" target="_blank" className="mb-5 md:mb-0">
-          <img src={iconInstagram} alt="Instagram" className="w-10" />   
-        </a>        
-        <Link to="/impressum" className="hover:underline">impressum</Link>
-        <Link to="/legal" className="hover:underline">legal notice</Link>        
-        <div className="md:ml-auto">
-          © {new Date().getFullYear()} Keingarten
-        </div>
-      </div>
+      </div>        
     </footer>
   );
 };
