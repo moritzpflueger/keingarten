e80d8cbb2ce791ba409b16c18200f3a06e2a5d95
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-07-26 16:51:44 +0200

    apply auto formatting

--- src/components/WeatherWidget.jsx
+++ src/components/WeatherWidget.jsx
@@ -1,47 +1,47 @@
-import React, { useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
+import React, { useEffect } from "react"
+import { useTranslation } from "react-i18next"
 
 const WeatherWidget = () => {
-  const { i18n } = useTranslation();
+  const { i18n } = useTranslation()
 
   useEffect(() => {
     const loadScript = () => {
-      const existingScript = document.getElementById('weatherwidget-io-js');
+      const existingScript = document.getElementById("weatherwidget-io-js")
       if (existingScript) {
-        existingScript.remove();
+        existingScript.remove()
       }
 
-      const script = document.createElement('script');
-      script.id = 'weatherwidget-io-js';
-      script.src = 'https://weatherwidget.io/js/widget.min.js';
-      document.body.appendChild(script);
-    };
+      const script = document.createElement("script")
+      script.id = "weatherwidget-io-js"
+      script.src = "https://weatherwidget.io/js/widget.min.js"
+      document.body.appendChild(script)
+    }
 
-    loadScript();
+    loadScript()
 
     // Cleanup function to remove the script when the component unmounts or language changes
     return () => {
-      const script = document.getElementById('weatherwidget-io-js');
+      const script = document.getElementById("weatherwidget-io-js")
       if (script) {
-        script.remove();
+        script.remove()
       }
-    };
-  }, [i18n.language]);
+    }
+  }, [i18n.language])
 
   return (
     <div className="w-[230px] max-h-20 relative scale-75 origin-left -translate-x-4 -translate-y-3 sm:-translate-y-1">
-      <a 
-        className="weatherwidget-io" 
-        href={`https://forecast7.com/${i18n.language}/48d819d16/stuttgart-feuerbach/`} 
+      <a
+        className="weatherwidget-io"
+        href={`https://forecast7.com/${i18n.language}/48d819d16/stuttgart-feuerbach/`}
         data-font="Monaco"
         data-mode="Current"
-        style={{ display: 'block', width: '100%', height: '100%' }}
+        style={{ display: "block", width: "100%", height: "100%" }}
       >
         Stuttgart, Feuerbach, Germany
-      </a>      
+      </a>
       <div className="absolute w-full h-full top-0 left-0"></div>
     </div>
-  );
-};
+  )
+}
 
-export default WeatherWidget;
+export default WeatherWidget
