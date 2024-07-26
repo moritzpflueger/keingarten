aea687c2498105642470051a58a322e3c0eb938e
Moritz Pflüger <moritzpflueger@googlemail.com>
2024-07-26 16:19:27 +0200

    apply auto formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,116 @@
-import './App.css'
-import { useEffect } from 'react'
-import { Link, useLocation } from 'react-router-dom'
-import { useTranslation, Trans } from 'react-i18next'
+import "./App.css"
+import { useEffect } from "react"
+import { Link, useLocation } from "react-router-dom"
+import { useTranslation, Trans } from "react-i18next"
 
-import Registration from './components/Registration'
-import Programme from './components/Programme'
-import FAQ from './components/FAQ'
-import InfoBox from './components/InfoBox'
+import Registration from "./components/Registration"
+import Programme from "./components/Programme"
+import FAQ from "./components/FAQ"
+import InfoBox from "./components/InfoBox"
 
 function App() {
-
   const { t, i18n } = useTranslation()
-  const location = useLocation();
+  const location = useLocation()
 
   // const formattedStartDate = new Date('2024-08-01').toLocaleDateString(i18n.language)
   // const formattedEndDate = new Date('2024-08-12').toLocaleDateString(i18n.language)
-  const formattedStartDate = new Date('2024-08-01').toLocaleDateString('de')
-  const formattedEndDate = new Date('2024-08-12').toLocaleDateString('de')
+  const formattedStartDate = new Date("2024-08-01").toLocaleDateString("de")
+  const formattedEndDate = new Date("2024-08-12").toLocaleDateString("de")
 
   useEffect(() => {
-    console.log('location', location.hash)
-    const hash = location.hash.replace('#', '');
+    console.log("location", location.hash)
+    const hash = location.hash.replace("#", "")
     if (hash) {
-      const element = document.getElementById(hash);
+      const element = document.getElementById(hash)
       if (element) {
-        const headerHeight = document.querySelector('header')?.offsetHeight || 0;
-        const elementPosition = element.getBoundingClientRect().top + window.pageYOffset;
-        const offsetPosition = elementPosition - headerHeight;
+        const headerHeight = document.querySelector("header")?.offsetHeight || 0
+        const elementPosition =
+          element.getBoundingClientRect().top + window.pageYOffset
+        const offsetPosition = elementPosition - headerHeight
 
         window.scrollTo({
           top: offsetPosition,
-          behavior: 'smooth'
-        });
+          behavior: "smooth",
+        })
       }
     }
-  }, [location]);    
+  }, [location])
 
   return (
     <div>
-      <section id="registration" className="p-5 mx-auto max-w-4xl">
+      <section
+        id="registration"
+        className="p-5 mx-auto max-w-4xl"
+      >
         <div className="py-10 grid grid-cols-1 md:grid-cols-2 gap-10">
           <div className="col-span-1">
-            <h1 
+            <h1
               className="font-serif text-8xl"
-              style={{ fontFamily: 'Phase, serif' }}
+              style={{ fontFamily: "Phase, serif" }}
             >
-              From A <br/> to Be
+              From A <br /> to Be
             </h1>
             <div className="flex gap-3 mt-5">
-              <InfoBox 
+              <InfoBox
                 type="question"
                 id="registration-question"
                 place="top"
               >
-                <Trans 
+                <Trans
                   i18nKey="registration.tooltip.question"
                   components={{
-                    LinkFaq: <Link to="/#faq" className="font-bold underline" />,
-                    LinkContact: <Link to="/contact" className="font-bold underline" />
+                    LinkFaq: (
+                      <Link
+                        to="/#faq"
+                        className="font-bold underline"
+                      />
+                    ),
+                    LinkContact: (
+                      <Link
+                        to="/contact"
+                        className="font-bold underline"
+                      />
+                    ),
                   }}
                 />
               </InfoBox>
-              <InfoBox 
+              <InfoBox
                 type="info"
                 id="registration-info"
                 place="top"
               >
-                <Trans 
-                  i18nKey="registration.tooltip.info"
-                />
-              </InfoBox>              
+                <Trans i18nKey="registration.tooltip.info" />
+              </InfoBox>
             </div>
             <p className="text-2xl my-10">
-              { formattedStartDate } - { formattedEndDate}
+              {formattedStartDate} - {formattedEndDate}
             </p>
             <p className="text-2xl">
               <Trans i18nKey="heroDescription">
-                Workshops with <a className="whitespace-nowrap underline" href="https://www.muenchner-kammerspiele.de/de/wir/13127-ula-liagait" target="_blank">Ula Liagaite</a>, 
-                AnttoLogy, <a className="whitespace-nowrap underline" href="https://www.tatianaheuman.com/" target="_blank">Tatiana Heumann</a> 
-                and <a className="whitespace-nowrap underline" href="https://florenciacurci.com/" target="_blank">Florencia Curci</a>
+                Workshops with{" "}
+                <a
+                  className="whitespace-nowrap underline"
+                  href="https://www.muenchner-kammerspiele.de/de/wir/13127-ula-liagait"
+                  target="_blank"
+                >
+                  Ula Liagaite
+                </a>
+                , AnttoLogy,{" "}
+                <a
+                  className="whitespace-nowrap underline"
+                  href="https://www.tatianaheuman.com/"
+                  target="_blank"
+                >
+                  Tatiana Heumann
+                </a>
+                and{" "}
+                <a
+                  className="whitespace-nowrap underline"
+                  href="https://florenciacurci.com/"
+                  target="_blank"
+                >
+                  Florencia Curci
+                </a>
               </Trans>
             </p>
           </div>
@@ -86,7 +118,7 @@ function App() {
             <Registration />
           </div>
         </div>
-      </section>  
+      </section>
       <Programme />
       <FAQ />
     </div>
